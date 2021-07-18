8df21196223e73f1e258b0e834bd2075e191508c
Wojciech Kwolek <me@irth.pl>
2021-07-18 17:50:35 +0200

    conversation layout

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import "./App.css";
 
 import ConversationList from "./ConversationList";
 
-import { useState } from "react";
+import {useState} from "react";
+import Conversation from "./Conversation";
 
 function App() {
-  const [route, setRoute] = useState({
-    name: "list",
-  });
-  return (
-    <div className="App">
-      {route.name === "list" ? <ConversationList /> : false}
-      {route.name === "conversation" ? (
-        <div className="Conversation"></div>
-      ) : (
-        false
-      )}
-    </div>
-  );
+    const [route, setRoute] = useState({
+        name: "conversation",
+    });
+    return (
+        <div className="App">
+            {route.name === "list" ? <ConversationList /> : false}
+            {route.name === "conversation" ? (
+                <Conversation />
+            ) : (
+                    false
+                )}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Conversation.js
@@ -0,0 +1,11 @@
+import "./Conversation.css";
+
+function Conversation() {
+    return <div className="Conversation">
+        <div className="topbar">topbar</div>
+        <div className="messages">messages</div>
+        <div className="input">input</div>
+    </div>;
+}
+
+export default Conversation;
